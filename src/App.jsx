b0d817372db2ba09d0760aac1075c6723fe8b754
shivam550 <shivamsingh51609@gmail.com>
2023-12-05 18:12:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='relative'> 
+      nav
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+    </main>
+  )
+}
+
+export default App
